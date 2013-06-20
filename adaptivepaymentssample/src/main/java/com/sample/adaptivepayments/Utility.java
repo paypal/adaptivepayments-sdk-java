4c7858e4b28738f3f497c4f970648f80205233eb
tkanta <tarininegi@rediffmail.com>
2013-06-20 05:25:49 -0700

    MODE parameter added

--- adaptivepaymentssample/src/main/java/com/sample/adaptivepayments/Utility.java
+++ adaptivepaymentssample/src/main/java/com/sample/adaptivepayments/Utility.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class Utility {
 	
-	private static final String MODE = "mode";
+	private static final String MODE_PARAM_KEY = "mode";
+	private static final String MODE = "sandbox";
 	
 	public static final Map<String,String> getSignatureConfig(){
 		Map<String,String> configMap = new HashMap<String,String>();
@@ -13,7 +14,7 @@ public class Utility {
 		configMap.put("acct1.Password", "WX4WTU3S8MY44S7F");
 		configMap.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
 		configMap.put("acct1.AppId", "APP-80W284485P519543T");
-		configMap.put(MODE, "sandbox");
+		configMap.put(MODE_PARAM_KEY, MODE);
 		return configMap;
 	}
 	
@@ -24,7 +25,7 @@ public class Utility {
 		configMap.put("acct2.CertKey", "password");
 		configMap.put("acct2.CertPath", "resource/sdk-cert.p12");
 		configMap.put("acct2.AppId", "APP-80W284485P519543T");
-		configMap.put(MODE, "sandbox");
+		configMap.put(MODE_PARAM_KEY, MODE);
 		return configMap;
 	}
 }
